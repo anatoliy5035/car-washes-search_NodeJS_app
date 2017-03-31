0b9107435f0d9e1c89c8f46fd0e1e0aa3af067af
Anatoliy <a.bygyb@gmail.com>
2017-03-31 20:51:52 +0300

    delete unused code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const routes = require('express').Router();
 const input = require('./input');
-const search = require('./search');
 const searchPage = require('./search-page');
 
 routes.get('/', input);
 routes.get('/search', searchPage);
-routes.post('/getCords', search);
 
 module.exports = routes;
--- routes/search.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-    let convertedCords = {
-        lat : 49.428180,
-        lng : 32.039510
-    };
-    let address = Object.keys(req.body)[0];
-    res.send(convertedCords);
-};
